1b406f9a4fa9c70b97a8dc1ba2d3eaa988f6ed97
Lisiak_A <lisiakandrei@gmail.com>
2020-08-16 21:06:16 +0300

    header Navbar profile created

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+		<header className="header">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/POL_COA_Lis.svg/1200px-POL_COA_Lis.svg.png" />
+		</header>
+   );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+   return (
+		<div className="content">
+			<div>
+				<img src="https://3.bp.blogspot.com/-2FAWxivXUUY/UwffyVvuYnI/AAAAAAAAPZ0/22AOcErMB2Q/s1600/davinci-panorama.jpg" />
+			</div>
+			<div>ava+description</div>
+			<div>
+				My posts
+				<div>New post</div>
+				<div>post 1</div>
+				<div>post 2</div>
+			</div>
+		</div>
+   );
+}
+
+export default Profile;
