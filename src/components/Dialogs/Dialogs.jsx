255502446b1c0158a7cea331fa4a8220ce3dfd77
Lisiak_A <lisiakandrei@gmail.com>
2020-08-24 14:40:52 +0300

    render.js + rerenderEntireTree

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
-import DialogItem from './DialogItem/DialogItem.jsx'
-import Message from './Message/Message.jsx'
-
+import DialogItem from './DialogItem/DialogItem.jsx';
+import Message from './Message/Message.jsx';
 
 const Dialogs = (props) => {
+	let dialogsElements = props.stateData.dialogsData.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />);
 
-	let dialogsElements = props.stateData.dialogsData
-		.map((dialog) => <DialogItem name={dialog.name} id={dialog.id} />);
-	
-	let messagesElements = props.stateData.messagesData
-		.map((message) => <Message message={message.message} />);
+	let messagesElements = props.stateData.messagesData.map((message) => <Message message={message.message} />);
 
-
-   return (
+	return (
 		<div className={classes.dialogs}>
-			<div className={classes.dialogsItems}>
-
-				{dialogsElements}
-
-				
-			</div>
-			<div className={classes.messages}>
-
-				{messagesElements}
-
-			</div>
+			<div className={classes.dialogsItems}>{dialogsElements}</div>
+			<div className={classes.messages}>{messagesElements}</div>
 		</div>
-   );
-}
+	);
+};
 
-export default Dialogs
+export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,17 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-
-	let postsElements = props.postsData.map((post) => (
-		<Post message={post.post} likesCount={post.likesCount} />
-	));
+	let postsElements = props.postsData.map((post) => <Post message={post.post} likesCount={post.likesCount} />);
 
 	let newPostElement = React.createRef();
 
 	let addPost = () => {
 		let text = newPostElement.current.value;
 		props.addPost(text);
-	}
+		newPostElement.current.value = '';
+	};
 
-   return (
+	return (
 		<div className={classes.mypostBlock}>
 			<h3>My posts</h3>
 			<div>
@@ -23,14 +21,12 @@ const MyPosts = (props) => {
 					<textarea ref={newPostElement}></textarea>
 				</div>
 				<div>
-					<button onClick={ addPost }>Add Post</button>
+					<button onClick={addPost}>Add Post</button>
 				</div>
 			</div>
-			<div className={classes.posts}>
-				{postsElements}
-			</div>
+			<div className={classes.posts}>{postsElements}</div>
 		</div>
-   );
-}
+	);
+};
 
-export default MyPosts
+export default MyPosts;
